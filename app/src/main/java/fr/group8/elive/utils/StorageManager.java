c58c3b3bd884feb99b5f27d2a8f07cbf97803dc6
Michaël Scherer <michael.scherer@viacesi.fr>
2016-01-27 17:12:45 +0100

    Storage updates to filestream management

--- app/src/main/java/fr/group8/elive/utils/StorageManager.java
+++ app/src/main/java/fr/group8/elive/utils/StorageManager.java
@@ -1,9 +1,23 @@
 package fr.group8.elive.utils;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteException;
+import android.util.Base64;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import fr.group8.elive.models.DataUser;
+import ru.noties.storm.DatabaseManager;
+import ru.noties.storm.Storm;
+import ru.noties.storm.exc.StormException;
+import ru.noties.storm.query.Selection;
 
 /**
  * Created by psyko on 18/01/16.
@@ -15,23 +29,31 @@ public class StorageManager {
         return ourInstance;
     }
 
-    public static String appFileName = "elive_app_data";
+    public static String appFileName = "elive_app_data.json";
 
+    private final Boolean IS_DEBUG = true;
+    private final int DB_VERSION = 1;
     private FileOutputStream fileOutputStream;
+    private DatabaseManager dbManager;
+    private boolean dbReady;
+
 
     private StorageManager() {
         fileOutputStream = null;
-
+        dbManager = null;
+        dbReady = false;
     }
 
     public boolean openFileStorage(Context context, String fileName) {
-        if (fileName == null || fileName.isEmpty() || fileOutputStream == null) return false;
 
-        String innerFileName;
-        if (fileName.isEmpty())
-            innerFileName = appFileName;
-        else
+        if (fileOutputStream == null) return false;
+
+        String innerFileName = appFileName;
+
+        if (fileName == null || fileName.isEmpty())
+        {
             innerFileName = fileName;
+        }
 
         try {
             fileOutputStream = context.openFileOutput(innerFileName, Context.MODE_APPEND);
@@ -43,7 +65,9 @@ public class StorageManager {
     }
 
     public void appendFileStorage(String text) {
-        if (text == null || text.isEmpty() || fileOutputStream == null) return;
+        if (text == null
+                || text.isEmpty()
+                || fileOutputStream == null) return;
 
         try {
             fileOutputStream.write(text.getBytes());
@@ -62,5 +86,55 @@ public class StorageManager {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public void createDataBase(Context context) {
+        if (!dbReady)
+        {
+            Storm.getInstance().init(context, IS_DEBUG);
+            dbReady = true;
+        }
+    }
+
+    public void initiateDBManager(Context context, String dbName) {
+        this.closeDBManager();
+
+        dbManager = new DatabaseManager(context, dbName, DB_VERSION, null);
+
+        try {
+            dbManager.open();
+        } catch (SQLiteException e) {
+            dbManager = null;
+            e.printStackTrace();
+        }
+    }
+
+    public void closeDBManager() {
+        if (dbManager != null
+                && dbManager.isOpen()) {
+            dbManager.close();
+            dbManager = null;
+        }
+    }
+
+    public void insert(DataUser user) {
+        if (!this.dbManager.isOpen())
+            throw new SQLiteException("DbManager has not been instanciated yet");
+        try {
+            Storm.newInsert(dbManager).insert(user);
+        } catch (StormException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insert(String uniqId, String fileName) {
+        this.insert(new DataUser(uniqId, fileName));
+    }
+
+    public DataUser select(String uniqId) {
+        if (!this.dbManager.isOpen())
+            throw new SQLiteException("DbManager has not been instanciated yet");
+
+        return Storm.newSelect(dbManager).query(DataUser.class, Selection.eq("uniqId", uniqId));
     }
 }
