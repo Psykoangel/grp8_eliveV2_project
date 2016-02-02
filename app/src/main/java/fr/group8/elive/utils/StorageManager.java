7816e6835cc323d699a11697995d95a8c48d60fb
Michaël Scherer <michael.scherer@viacesi.fr>
2016-02-02 09:57:58 +0100

    StorageManager : Open Json File and Add Object functionnalities

--- app/src/main/java/fr/group8/elive/utils/StorageManager.java
+++ app/src/main/java/fr/group8/elive/utils/StorageManager.java
@@ -2,16 +2,22 @@ package fr.group8.elive.utils;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteException;
-import android.util.Base64;
+import android.util.JsonReader;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import fr.group8.elive.models.DataUser;
 import ru.noties.storm.DatabaseManager;
@@ -33,6 +39,7 @@ public class StorageManager {
 
     public final Boolean IS_DEBUG = true;
     public final int DB_VERSION = 1;
+    public final int FILE_MAX_USER_NUMBER = 500;
     private DatabaseManager dbManager;
     private boolean dbReady;
 
@@ -42,17 +49,100 @@ public class StorageManager {
         dbReady = false;
     }
 
-    public void loadJsonDataFile() {
+    public void loadJsonDataFile(String fileName) {
 
     }
 
-    public void addDataToJsonFile() {
+    private void addDataToJsonFile(File fileJson) {
 
+        String strFileJson = null;
+        JSONObject jsonObj = null;
+
+        try {
+            strFileJson = getStringFromFile(fileJson.toString());
+            jsonObj = new JSONObject(strFileJson);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Gson gson = new Gson();
+        JsonParser jsonParser = new JsonParser();
+
+        // writeJsonFile(fileJson, jsonObj.toString());
+
+    }
+
+    private String getStringFromFile (String filePath) throws Exception {
+
+        File fl = new File(filePath);
+        FileInputStream fin = new FileInputStream(fl);
+        String res = convertStreamToString(fin);
+        fin.close();
+
+        return res;
+    }
+
+    private String convertStreamToString(InputStream is) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        
+        return sb.toString();
+    }
+
+    private void writeJsonFile(File file, String json)
+    {
+        BufferedWriter bufferedWriter = null;
+        try {
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(json);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedWriter != null){
+                    bufferedWriter.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     public String searchDataFromJsonFile() {
 
+        try {
+            String user = readJsonStream(new FileInputStream(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "User";
+    }
+
+    private String readJsonStream(InputStream is) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+
+        try {
+
+            return "User";
+        } finally {
+            reader.close();
+        }
     }
 
     public void createDataBase(Context context) {
